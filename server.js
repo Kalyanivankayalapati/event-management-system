a3d6b49025bea94b9c42e368242dd55f416d8849
Kalyani <kalyani630126@gmail.com>
2026-03-03 13:58:40 +0530

    Added DB port for Railway

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ app.use(cors());
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "client")));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "login.html"));
+});
 
 // ================= DATABASE CONNECTION =================
 const db = mysql.createConnection({
