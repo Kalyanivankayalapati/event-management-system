3e1c43542da5d1382d32961ec5119259ac9677df
Kalyani <kalyani630126@gmail.com>
2026-03-03 13:27:58 +0530

    Updated port and env config for deployment

--- server.js
+++ server.js
@@ -236,6 +236,8 @@ app.get('/attendance-summary/:event_id', (req, res) => {
 });
 
 // ================= SERVER START =================
-app.listen(5000, () => {
-    console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
 });
