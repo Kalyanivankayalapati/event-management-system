122108c7dd62d144a4a77baf5a290502db7be446
Kalyani <kalyani630126@gmail.com>
2026-03-03 13:46:19 +0530

    Added DB port for Railway

--- server.js
+++ server.js
@@ -13,8 +13,10 @@ const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: process.env.PORT   // ✅ ADD THIS LINE
 });
+
 db.connect(err => {
     if (err) {
         console.log("Database connection failed:", err);
